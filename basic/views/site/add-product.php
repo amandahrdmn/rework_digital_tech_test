017fa1b6f7c1ffb7ab0e15a0c78e9c7a3ac3707a
Amanda Hardman <amandahrdmn@yahoo.com>
2020-11-11 11:50:27 +0000

    Removed unneeded container div.

--- basic/views/site/add-product.php
+++ basic/views/site/add-product.php
@@ -18,41 +18,39 @@ $this->params['breadcrumbs'][] = $this->title;
         This is the form for adding a product. Please note that you cannot add previously added products.
     </p>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
+    <div class="row">
+        <div class="col-xs-12">
 
-                <?php $form = ActiveForm::begin(['id' => 'add-product-form']); ?>
+            <?php $form = ActiveForm::begin(['id' => 'add-product-form']); ?>
 
-                <?= $form->field($model, 'productName')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'productName')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'price')->textInput(['type' => 'text',
-                    'maxlength' => 13,
-                    'id' => 'priceField',
-                ])?>
+            <?= $form->field($model, 'price')->textInput(['type' => 'text',
+                'maxlength' => 13,
+                'id' => 'priceField',
+            ])?>
 
-                <?= $form->field($model, 'quantity')->textInput(['type' => 'text',
-                    'maxlength' => 11,
-                    'id' => 'quantityField',
-                ]) ?>
+            <?= $form->field($model, 'quantity')->textInput(['type' => 'text',
+                'maxlength' => 11,
+                'id' => 'quantityField',
+            ]) ?>
 
-                <?= $form->field($model, 'categories')->checkboxList([
-                    1 => 'checkbox 1',
-                    2 => 'checkbox 2',
-                    3 => 'checkbox 2',
-                    4 => 'checkbox 2',
-                    5 => 'checkbox 2'
-                ]);
-                ?>
+            <?= $form->field($model, 'categories')->checkboxList([
+                1 => 'checkbox 1',
+                2 => 'checkbox 2',
+                3 => 'checkbox 2',
+                4 => 'checkbox 2',
+                5 => 'checkbox 2'
+            ]);
+            ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'add-product-button']) ?>
-                    <a class="btn btn-danger" href="index.php">Back</a>
-                </div>
-
-                <?php ActiveForm::end(); ?>
-
+            <div class="form-group">
+                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'add-product-button']) ?>
+                <a class="btn btn-danger" href="index.php">Back</a>
             </div>
+
+            <?php ActiveForm::end(); ?>
+
         </div>
     </div>
 </div>
